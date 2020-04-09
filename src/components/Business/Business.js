f41ad570cb21045fedcbf36a6d14577148fd4b65
Marc Pàmpols <mpampols@gmail.com>
2020-04-09 23:59:04 +0200

    Update part 3

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -6,7 +6,7 @@ class Business extends React.Component {
     return (
       <div className="Business">
         <div className="image-container">
-          <img src="{business.name}" alt="{business.imageSrc}" />
+          <img src={this.props.business.imageSrc} alt={this.props.business.imageSrc} />
         </div>
         <h2>{this.props.business.name}</h2>
         <div className="Business-information">
